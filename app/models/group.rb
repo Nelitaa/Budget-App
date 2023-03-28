23127f7cf9f6a84bc6b572c4b45cc88b0df47658
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-28 14:12:14 -0400

    Add validations and associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+  has_many :group_expenses, dependent: :destroy
+  has_many :expenses, through: :group_expenses
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: true
 end
--- app/models/group_movement.rb
+++ app/models/group_movement.rb
@@ -1,2 +1,4 @@
 class GroupMovement < ApplicationRecord
+  belongs_to :group
+  belongs_to :expense
 end
